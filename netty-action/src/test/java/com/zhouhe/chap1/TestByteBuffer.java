7f6c1c5829a40f83de80a4bfd1ee1917067867ed
jiejiecool <jiejiecool@163.com>
2021-12-13 23:06:17 +0800

    FileChannel读文件并使用一个很小的数组循环读取

--- netty-action/src/test/java/com/zhouhe/chap1/TestByteBuffer.java
+++ netty-action/src/test/java/com/zhouhe/chap1/TestByteBuffer.java
@@ -1,13 +1,39 @@
 package com.zhouhe.chap1;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
-import java.io.FileInputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 @Slf4j
 public class TestByteBuffer {
-    public static void main(String[] args) {
-        //FileInputStream
+
+    //读取文件中的内容，支持超过数组长度，循环往数组里写数据
+    @Test
+    public void testRWFile() {
+        try (RandomAccessFile file = new RandomAccessFile("test.txt", "rw")) {
+            FileChannel channel = file.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+            log.info("byteBuffer.info, position:{}, limit:{}, capacity:{}", byteBuffer.position(), byteBuffer.limit(), byteBuffer.position());
+
+            int i = -1;
+            while ((i = channel.read(byteBuffer)) != -1) {
+                byteBuffer.flip();
+                while (byteBuffer.hasRemaining()) {
+                    System.out.print((char) byteBuffer.get());
+                }
+                System.out.println("");
+                byteBuffer.clear();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
