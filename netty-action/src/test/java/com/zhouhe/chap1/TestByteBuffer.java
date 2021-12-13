a800a4750252a3b12302e9cf9709ab1f070be80e
zhoujie <zhoujie>
2021-12-13 22:21:58 +0800

    添加netty模块

---
+++ netty-action/src/test/java/com/zhouhe/chap1/TestByteBuffer.java
@@ -0,0 +1,13 @@
+package com.zhouhe.chap1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.nio.channels.FileChannel;
+
+@Slf4j
+public class TestByteBuffer {
+    public static void main(String[] args) {
+        //FileInputStream
+    }
+}
